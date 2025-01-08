9fe5d2740ba29db8ff04558d7a8a2e2d6b6d02bf
Namiq Allahverdov <superhanter@bk.ru>
2025-01-08 16:04:50 +0400

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./assets/components/HomePage";
+import { AboutPage } from "./assets/components/AboutPage";
+import { ProductsPage } from "./assets/components/ProductsPage";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Welcome</h1>
-        <h2>Some text</h2>
-        <h3>Hello world</h3>
-      </div>
+      <nav>
+        <ul className="menu-list">
+          <NavLink className="menu-item" to="/">
+            <li>Home</li>
+          </NavLink>
+          <NavLink className="menu-item" to="/about">
+            <li>About</li>
+          </NavLink>
+
+          <NavLink className="menu-item" to="/products">
+            <li>Products</li>
+          </NavLink>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/assets/components/ProductsPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const productsData = [
+  {
+    id: 1,
+    title: "Toaster",
+    description: "",
+  },
+];
+
+export const ProductsPage = () => {
+  return (
+    <div>
+      <h1>ProductsPage</h1>
+      <ul></ul>
+    </div>
+  );
+};
